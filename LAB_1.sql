35d6b8948c355ec0d9ac2d68124c2bac2cc2def9
MerrittK <65367913+MerrittK@users.noreply.github.com>
2025-01-29 22:12:46 -0500

    Update LAB_1.sql

--- LAB_1.sql
+++ LAB_1.sql
@@ -1,63 +1,70 @@
 use ap;
-# 1
-SELECT * FROM vendors ORDER BY vendor_id;
 
-# 2
+-- 1 Selects all from vendors
+SELECT * 
+FROM vendors 
+ORDER BY vendor_id;
+
+-- 2 Selects columns from vendors in CA
 SELECT vendor_name, vendor_phone, vendor_city
 FROM vendors 
 WHERE vendor_state = 'CA' 
 ORDER BY vendor_name;
     
-# 3
+-- 3 Gets
 SELECT invoice_id, invoice_total, invoice_date 
 FROM invoices 
 ORDER BY invoice_total DESC;
-# 4 
+
+-- 4  Gets 3rd to 9th items in ascending.
 SELECT invoice_id, invoice_total
 FROM invoices 
-ORDER BY invoice_total
-LIMIT 3, 6;
+ORDER BY invoice_total ASC -- explicit ASC for clarity
+LIMIT 3, 6; -- gets 6 lines after 3, equivilant to getting lines 3-9
 
-#5
-SELECT invoice_id, invoice_total, invoice_total - payment_total AS "Remaining Balance" 
+-- 5 -Aliases arithmatic operation to "remaining balance"
+SELECT invoice_id, invoice_total, 
+	invoice_total - payment_total AS "Remaining Balance" 
 FROM invoices 
 ORDER BY invoice_id;
     
-#6
+--6
 SELECT invoice_id, invoice_total, vendor_name, vendor_phone 
 FROM vendors INNER JOIN invoices
 	ON vendors.vendor_id = invoices.vendor_id
 ORDER BY invoice_id;
 
-#7
-SELECT invoice_id, invoice_total, vendor_name, vendor_phone
-FROM vendors LEFT JOIN invoices
+--7
+SELECT vendor_name, invoice_id
+FROM vendors LEFT JOIN invoices --left join gets vendors who do not have linked invoices
 	ON vendors.vendor_id = invoices.vendor_id
 ORDER BY vendor_name;
 
-#8 - This one's instructions has the wrong names for the columns and order by
+--8 the assignment "employee_last_name" doesn't exist, using last_name from ex.employees
 SELECT last_name, department_name
-FROM ex.departments RIGHT JOIN ex.employees
-	ON ex.departments.department_number= ex.employees.department_number 
+FROM ex.employees LEFT JOIN ex.departments
+	ON ex.employees.department_number = ex.departments.department_number
 ORDER BY departments.department_number;
 
-#9
+--9 gets a concatanated fullname as full_name
 SELECT CONCAT(first_name,' ' , last_name) as full_name , vendor_name
-FROM vendor_contacts JOIN vendors
+FROM vendor_contacts INNER JOIN vendors -- we are only interested in cases with a vendor contact
 	ON vendor_contacts.vendor_id = vendors.vendor_id;
 
-#10
+--10
 SELECT first_name FROM ex.employees
-UNION
+UNION --Union gets unique by default.
 SELECT rep_first_name FROM ex.sales_reps;
 
-#11
+--11
 SELECT invoice_id, invoice_total, vendor_name, terms_description
 FROM invoices
-	LEFT JOIN vendors
-		ON invoices.vendor_id = vendors.vendor_id
-	LEFT JOIN terms
-		ON invoices.terms_id = terms.terms_id
-ORDER BY invoice_id
-    ;
+    INNER JOIN vendors
+		ON invoices.vendor_id = vendors.vendor_id --attach everything via invoice.id.
+	INNER JOIN terms -- Hypothetically, IG we could have invoices without valid terms or vendors, but that would most
+			 --likely be a sign of an error.
+		ON invoices.terms_id = terms.terms_id 
+ORDER BY invoice_id;
+    
+
     
